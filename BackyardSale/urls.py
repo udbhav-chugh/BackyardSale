58a35e8515a1c4069264b74195ab4dd5eb60acbe
Udbhav Chugh <udbha170123055@iitg.ac.in>
2019-03-02 19:33:14 +0530

    added dashboard url

--- BackyardSale/urls.py
+++ BackyardSale/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home),
+    path('dashboard/', include('DashBoard.urls')),
 ]
